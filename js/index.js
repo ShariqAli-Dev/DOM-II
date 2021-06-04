c434e9d5d9ca953d96029de81c497140dd13397e
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-03 23:27:07 -0400

    + Mouseover Event Listener

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
-// Your code goes here
+ // Your code goes here
 // HELELOOLOLO
+/* Event
+  * `mouseover`
+  * `keydown`
+  * `wheel`
+  * `load`
+  * `focus`
+  * `resize`
+  * `scroll`
+  * `select`
+  * `dblclick`
+  * `drag / drop`
+*/
+
+// ### Variables ### //
+const introImg = document.querySelector('.intro img');
+const introH2 = document.querySelector('.intro h2');
+const introP = document.querySelector('.intro p');
+
+
+// ### Mouseover ### // 
+introImg.addEventListener('mouseover', event => {
+  introImg.src = 'https://i.ytimg.com/vi/v_cpPMjE0vU/maxresdefault.jpg';
+  introImg.alt = 'picture of the cutest panda ever';
+  
+  introH2.textContent = 'Welcome To The Panda Zone!';
+  
+  introP.textContent = 'PANDA POO PANDA POO POO PANDA BANG POO PAW POW POW POAD';
+})
+
+// ### Keydown ###
+
