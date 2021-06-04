0015c1d9fcb8c4b41fcf26da960d4cc46180fe1c
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-04 00:03:19 -0400

    + Keydown Event Listener

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
- // Your code goes here
-// HELELOOLOLO
-/* Event
-  * `mouseover`
-  * `keydown`
-  * `wheel`
-  * `load`
-  * `focus`
-  * `resize`
-  * `scroll`
-  * `select`
-  * `dblclick`
-  * `drag / drop`
-*/
-
 // ### Variables ### //
 const introImg = document.querySelector('.intro img');
 const introH2 = document.querySelector('.intro h2');
 const introP = document.querySelector('.intro p');
+
+let keydownArray = [];
 
 
 // ### Mouseover ### // 
@@ -29,5 +16,38 @@ introImg.addEventListener('mouseover', event => {
   introP.textContent = 'PANDA POO PANDA POO POO PANDA BANG POO PAW POW POW POAD';
 })
 
+
 // ### Keydown ###
+// tracks keypresses, every 5 there is an alert message. the keypresses are saved to an array and every 5 in an array there is included a space
+document.addEventListener('keydown', event => {
+  keydownArray.push(event.key);
+  if((keydownArray.length % 5 === 0) && (keydownArray.length !== 0)) {
+    alert(`You have pressed a total of ${keydownArray.length} keys. Which are: \n ${keydownArray}`);
+  }
+})
+
+
+// ### Wheel ### //
+
+
+// ### Load ### //
+
+
+// ### Focus ### //
+
+
+// ### Resize ### //
+
+
+// ### Scroll ### //
+
+
+// ### Select ### //
+
+
+// ### dbClick ### //
+
+
+// ### Drag / Drop ### //
+
 
